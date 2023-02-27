664a88bed5b6e6017a67090d71d5f0b0845e1e29
sanisaha <ratulcvasu@gmail.com>
2023-02-28 00:29:25 +0200

    game completed and deplpoyed

---
+++ main.js
@@ -0,0 +1,79 @@
+const circles = document.querySelectorAll('.circle')
+const message = document.querySelector('#message')
+const startButton = document.querySelector('#start')
+const endButton = document.querySelector('#end')
+const closeButton = document.querySelector('#close')
+const scoreStart = document.querySelector('.score')
+const scoreEnd = document.querySelector('.scoreEnd')
+const overlay = document.querySelector('.overlay')
+let score = 0;
+let active = 0;
+let timer;
+let pace = 800;
+let rounds = 0;
+let gameSound = new Audio('gameSound.wav')
+let clickSound = new Audio('clickSound.wav')
+let endSound = new Audio('endSound.wav')
+circles.forEach((circle, i) => {
+    circle.addEventListener('click', () => clickCircle(i));   
+})
+const getRndInt = (min, max) => Math.floor(Math.random()*(max-min+1)) + min
+const clickCircle = (i) => {
+    clickSound.play()
+    if (i !== active){
+        return endGame()
+    }
+    score += 10
+    rounds = 0
+    scoreStart.textContent = score
+}
+const enableCircles = () =>{
+    circles.forEach(circle => {
+        circle.style.pointerEvents = 'auto'
+    })
+}
+const startGame = () => {  
+    if(pace >= 800) {
+        gameSound.play()
+    }
+    if(rounds >= 4) {
+        return endGame()
+    }
+    startButton.classList.add('hidden')
+    endButton.classList.remove('hidden')
+    enableCircles()
+    const nextActive = pickNew(active)
+    circles[nextActive].classList.toggle('active')
+    circles[active].classList.remove('active')
+    active = nextActive
+    timer = setTimeout(startGame, pace)
+    pace -= 2
+    rounds++
+    function pickNew(active){
+        const nextActive = getRndInt(0,3)
+        if (nextActive !== active){
+            return nextActive
+        } else {            
+            return pickNew(active)
+        }
+    }
+}
+const endGame = () => {
+    endSound.play()
+    scoreEnd.textContent = score
+    if (score < 50) {
+        message.textContent = 'You need more practice'
+    } else {
+        message.textContent = 'well done! better luck next time'
+    }
+    endButton.classList.remove('hidden')
+    startButton.classList.add('hidden')
+    overlay.style.visibility = 'visible'
+    clearTimeout(timer)
+}
+const resetGame = () => {
+    window.location.reload()
+}
+startButton.addEventListener('click', startGame)
+endButton.addEventListener('click', endGame)
+closeButton.addEventListener('click', resetGame)
